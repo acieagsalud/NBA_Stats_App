42248d00111039867f43dd8b322cb72b418cd13c
acieagsalud <aciejohn@gmail.com>
2020-10-14 14:49:04 -0500

    Added api_id reference column for Player to be used in Season stat API calls

---
+++ db/migrate/20201014193533_add_api_id_to_players.rb
@@ -0,0 +1,5 @@
+class AddApiIdToPlayers < ActiveRecord::Migration[6.0]
+  def change
+    add_column :players, :api_id, :integer
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_14_172931) do
+ActiveRecord::Schema.define(version: 2020_10_14_193533) do
 
   create_table "games", force: :cascade do |t|
     t.datetime "date"
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 2020_10_14_172931) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "photo"
+    t.integer "api_id"
     t.index ["team_id"], name: "index_players_on_team_id"
   end
 
