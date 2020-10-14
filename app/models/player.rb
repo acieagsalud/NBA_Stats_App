b3202505c7c97bcf0e3e5f3ca62db7df84d17638
acieagsalud <aciejohn@gmail.com>
2020-10-14 14:48:01 -0500

    Fixed validation typo for Player and Team models

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,7 +1,7 @@
 class Player < ApplicationRecord
   belongs_to :team
 
-  validates :first_name, :last_name, presence :true
+  validates :first_name, :last_name, presence: true
   validates :weight, numericality: { only_integer: true}
   validates :height_feet, numericality: { only_integer: true}
   validates :height_inches, numericality: { only_integer: true}
