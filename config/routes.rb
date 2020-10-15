af90b59bffc3d14be04cee401e521ad512605fbd
acieagsalud <aciejohn@gmail.com>
2020-10-14 23:53:22 -0500

    Added controllers for players, games, seasons and added them to navigation routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
-  get 'about/index'
+  get 'seasons/index'
+  get 'seasons/show'
+  get 'games/index'
+  get 'games/show'
+  # get 'players/index'
+  # get 'players/show'
+  # get 'about/index'
   # get 'teams/show'
   # get 'teams/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
@@ -7,4 +13,7 @@ Rails.application.routes.draw do
   root to: "about#index"
 
   resources "teams", only: %i[index show]
+  resources "players", only: %i[index show]
+  resources "games", only: %i[index show]
+  resources "seasons", only: %i[index show]
 end
